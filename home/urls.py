4f8609a4d499fd9bcd6d072974de39b98bbe54bb
Atwiine Precious <preciousinshuti3@gmail.com>
2025-11-06 13:56:45 +0300

    Changed login details to index

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,8 @@ app_name = 'home'
 #borrowing the functionality of login from django
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    #path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('',views.LoginView.as_view(template_name='login.html'), name='login'),
+    # path('', auth_views.LoginView.as_view(template_name='index.html'), name='login'),
+    path('', views.Login.as_view(), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
     path('sales/add/',views.add_sale, name='add_sale'),
     path('sales/<int:pk>/edit/', views.edit_sale, name='edit_sale'),
