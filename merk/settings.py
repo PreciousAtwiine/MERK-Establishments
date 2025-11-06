4878db89b755553386f38a532acdbac678022fba
Atwiine Precious <preciousinshuti3@gmail.com>
2025-11-06 14:49:41 +0300

    Deployed on render

--- merk/settings.py
+++ merk/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-89xu1yq3s))psvh-+6evb6s%^wtoevpe@#8ipy!ukxgo(6zyv3'
+SECRET_KEY = os.environ.get('django-insecure-89xu1yq3s))psvh-+6evb6s%^wtoevpe@#8ipy!ukxgo(6zyv3')
+
+#SECRET_KEY = 'django-insecure-89xu1yq3s))psvh-+6evb6s%^wtoevpe@#8ipy!ukxgo(6zyv3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -134,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media (user uploads)
 MEDIA_URL = '/media/'
